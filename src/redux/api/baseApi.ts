a05514658b70bf5b05eccae0d15e612670cfa1b5
MrSachin7 <nepalghi@gmail.com>
2025-10-24 11:19:04 +0200

    Api changes based on prod / devb

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://localhost:5100/api";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "http://130.225.71.179:5100/api"   // prod
+    : "http://localhost:5100/api";      // dev
+
+// Or using env variables:
+// const baseUrl = process.env.NEXT_PUBLIC_API_URL!;
 
 export const baseApi = createApi({
   reducerPath: "api",
