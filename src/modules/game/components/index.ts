d75ce2b6760a5e8f98404fe97fae4c5476590fd6
MrSachin7 <nepalghi@gmail.com>
2025-11-14 14:18:59 +0100

    s

--- src/modules/game/components/index.ts
+++ src/modules/game/components/index.ts
@@ -1,5 +1,4 @@
 export { GameBoard } from "./GameBoard/GameBoard";
-export { GameHostControls } from "./GameHostControls/game-host-controls";
 export { PlayerInfoCard } from "./PlayerInfoCard/PlayerInfoCard";
 export { GamePauseButton } from "./GamePauseButton";
 export { GamePauseDialog } from "./GamePauseDialog";
